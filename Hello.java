e17230a56990a02957267445b682277c0322dd3b
Grace Wambui <wambuigm51@yahoo.com>
2026-05-09 17:19:34 +0300

    Add: data types and literals

--- Hello.java
+++ Hello.java
@@ -1,5 +1,30 @@
 public class Hello {
     public static void main(String a[]){
-        System.out.print("Hello, World!");
+        //Data types
+        String greetings = "Hellow, World!";
+        System.out.println(greetings);
+
+        int num1 = 7;
+        int num2 = 8;
+        int result = num1 + num2;
+        System.out.println(result);
+
+        double price = 568.22;
+        System.out.println(price);
+
+        float marks = 75.6f;
+        System.out.println(marks);
+
+        char grade = 'B'; //Must be a single quote 
+        System.out.println(grade);
+
+        boolean isJavaFun = true;
+        System.out.println(isJavaFun);
+
+        int age =25;
+        byte ageBytes = 25;
+        short sh = 25;
+        long l = 25l;
     }
 }
+
---
+++ Literals.java
@@ -0,0 +1,13 @@
+public class Literals {
+    public static void main(String a[]){
+        int num1 = 0b101;  //Outputs 5
+        System.out.println(num1);
+
+        int num2 = 0x7E; //Outputs 126
+        System.out.println(num2);
+
+        char c = 'a';
+        c++;
+        System.out.println(c);
+    }
+}
