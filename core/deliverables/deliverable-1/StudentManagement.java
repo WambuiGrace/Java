b0b7a5df08663747ca742b17d69abdbc73eaf9f7
Grace Wambui <wambuigm51@yahoo.com>
2026-05-13 19:19:11 +0300

    Add: Implement Student class and StudentManagement structure

---
+++ core/deliverables/deliverable-1/StudentManagement.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+private Student {
+    int id;
+    String name;
+    String course;
+    int age;
+}
+public Student(int id, String name, String course, int age);
+
+public class StudentManagement {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+    }
+}
