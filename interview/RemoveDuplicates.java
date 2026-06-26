3b88c4e419ec29ad3f264160aa28aa400f861fa3
Grace Wambui <wambuigm51@yahoo.com>
2026-06-26 18:10:06 +0300

    Add: Remove duplicates from an array

---
+++ interview/RemoveDuplicates.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 3, 4, 4, 5, 6, 7, 6, 3};
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : arr){
+            set.add(num);
+        }
+        System.out.println(set);
+    }
+}
